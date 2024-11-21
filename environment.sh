abb6cdcda2a9ecb37f7b7ff1ce7a5d46f1ad498f
Alonso <frstrtr@gmail.com>
2024-11-21 08:32:11 +0000

    Update runme.sh

---
+++ environment.sh
@@ -0,0 +1,1 @@
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/user0/go/pkg/mod/github.com/tonkeeper/tongo@v1.9.1-0.20240710061111-60ccf2c10f84/lib/linux/
---
+++ runme.sh
@@ -0,0 +1,4 @@
+# ACCOUNTS_FILE="numbers.txt" LITE_SERVERS="127.0.0.1:23815:gdJjRZ5p2Akp37O1Z2DI7jg9FAHiHy/J0BU0tL1S0Gg=" make run
+# ACCOUNTS_FILE="numbers.txt" LITE_SERVERS="127.0.0.1:23815:B0FEJ0PZE00s5/kCZnWzL/eDn1EPDjmSTpstZv3foRw=" make run
+# ACCOUNTS_FILE="numbers.txt" LITE_SERVERS="127.0.0.1:31251:S5NAn4Pm39cFXiUoIWLwYDwlgKAppX1vPfqIc5bWIsA=" make run
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/user0/go/pkg/mod/github.com/tonkeeper/tongo@v1.10.3/lib/linux/ ACCOUNTS_FILE="numbers.txt" LITE_SERVERS="192.168.86.20:31251:5SzTufXSiL+LrmU3u5rXjrp18hk9zSACbLsPQcC8cZ4=" make run
